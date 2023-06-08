6ce324cb306f82b07dc04dfdf06bbd05404da6ab
Chenkun Tan <shackleslay@gmail.com>
2023-06-08 15:55:48 +0800

    Prompt for task3 in inference.py & inference_qlora.py

--- finetune_qlora.py
+++ finetune_qlora.py
@@ -4,11 +4,124 @@ from datasets import load_dataset
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from peft.tuners.lora import LoraLayer
+import bitsandbytes as bnb
 import argparse
+from utils.device import DeviceMap  # 多卡微调
+
+def find_all_linear_names(args, model):
+    cls = bnb.nn.Linear4bit if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            
+    if 'lm_head' in lora_module_names: # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+def get_accelarate_model(args):
+    if args.full_finetune: assert args.bits in [16, 32]
+    compute_dtype = (torch.float16 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32))
+    model = AutoModelForCausalLM.from_pretrained(
+        "fnlp/moss-moon-003-sft",
+        load_in_4bit=args.bits == 4,
+        load_in_8bit=args.bits == 8,
+        device_map=DeviceMap("Moss").get(), # 多卡微调，单卡设置为"auto"
+        quantization_config=BitsAndBytesConfig(
+            load_in_4bit=args.bits == 4,
+            load_in_8bit=args.bits == 8,
+            llm_int8_threshold=6.0,
+            llm_int8_has_fp16_weight=False,
+            bnb_4bit_compute_dtype=compute_dtype,
+            bnb_4bit_use_double_quant=args.double_quant,
+            bnb_4bit_quant_type=args.quant_type # {'fp4', 'nf4'}
+        ),
+        torch_dtype=(torch.float32 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32)),
+        trust_remote_code=args.trust_remote_code,
+        )
+    if compute_dtype == torch.float16 and args.bits == 4:
+        major, minor = torch.cuda.get_device_capability()
+        if major >= 8:
+            print('='*80)
+            print('Your GPU supports bfloat16, you can accelerate training with the argument --bf16')
+            print('='*80)
+            
+    setattr(model, 'model_parallel', True)
+    setattr(model, 'is_parallelizable', True)
+    modules = find_all_linear_names(args, model)
+    
+    model.config.torch_dtype=(torch.float32 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32))
+    
+    if not args.full_finetune:
+        model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=args.gradient_checkpointing)
+    if args.gradient_checkpointing:
+        model.gradient_checkpointing_enable()
+    
+    config = LoraConfig(
+        r=args.lora_r,
+        lora_alpha=args.lora_alpha,
+        target_modules=modules,
+        lora_dropout=args.lora_dropout,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    
+    print(f'adding LoRA modules...')
+    model = get_peft_model(model, config)
+    
+    if args.gradient_checkpointing:
+        if hasattr(model, "enable_input_require_grads"):
+            model.enable_input_require_grads()
+        else:
+            def make_inputs_require_grad(module, input, output):
+                output.requires_grad_(True)
+            model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
+
+    for name, module in model.named_modules():
+        # if isinstance(module, LoraLayer):
+        #     if args.bf16:
+        #         module = module.to(torch.bfloat16)
+        if 'ln' in name:
+            module = module.to(torch.float32)
+        if 'lm_head' in name or 'embed_tokens' in name:
+            if hasattr(module, 'weight'):
+                if args.bf16 and module.weight.dtype == torch.float32:
+                    module = module.to(torch.bfloat16)
+    return model
 
 def train(args):
 
-    print("Instruction used for finetuning: ", args.instruction)
+    print("Instruction used for finetuning: ", args.instruction)    
+    
+#     bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.bfloat16
+# )
+#     model = AutoModelForCausalLM.from_pretrained(
+#         "fnlp/moss-moon-003-sft",
+#         trust_remote_code=True,
+#         quantization_config=bnb_config,
+#         device_map={"":0}
+#     )
+#     model.gradient_checkpointing_enable()
+#     model = prepare_model_for_kbit_training(model)
+
+        
+    # config = LoraConfig(
+    #     r=args.lora_r,
+    #     lora_alpha=args.lora_alpha,
+    #     target_modules=["q_proj", "v_proj"],
+    #     lora_dropout=args.lora_dropout,
+    #     bias="none",
+    #     task_type="CAUSAL_LM",
+    # # )
+
+    # model = get_peft_model(model, config)
+    model = get_accelarate_model(args)
 
     tokenizer = AutoTokenizer.from_pretrained(
         "fnlp/moss-moon-003-sft", 
@@ -16,36 +129,8 @@ def train(args):
         trust_remote_code=True
     )
     tokenizer.eos_token_id = 106068 # The eos_token_id of base model is 106028. We need map the eos token to <eom> (its token id is 106068)
-    
-    
-    bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-    model = AutoModelForCausalLM.from_pretrained(
-        "fnlp/moss-moon-003-sft",
-        trust_remote_code=True,
-        quantization_config=bnb_config,
-        device_map={"":0}
-    )
-    model.gradient_checkpointing_enable()
-    model = prepare_model_for_kbit_training(model)
-
-        
-    config = LoraConfig(
-        r=args.lora_r,
-        lora_alpha=args.lora_alpha,
-        target_modules=["q_proj", "v_proj"],
-        lora_dropout=args.lora_dropout,
-        bias="none",
-        task_type="CAUSAL_LM",
-    )
-
-    model = get_peft_model(model, config)
-
     tokenizer.pad_token_id = 0
+    
     data = load_dataset(
         "json",
         data_files=args.data
@@ -62,7 +147,7 @@ def train(args):
         input = data_point["input"]
         response = data_point["output"]
 
-        return f"<|Human|>:\n{instruction}\ninput:{input}\n<eoh>\n<|MOSS|>:{response}<eom>\n"
+        return f"<|Human|>:\n{instruction}\n{input}\n<eoh>\n<|MOSS|>:{response}<eom>\n"
 
 
     def tokenize(prompt):
@@ -102,7 +187,7 @@ def train(args):
             num_train_epochs=args.epochs,
             learning_rate=args.learning_rate,
             fp16=True,
-            logging_steps=1,
+            logging_steps=10,
             output_dir=args.output_dir,
             optim="paged_adamw_8bit"
         ),
@@ -122,11 +207,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--data', type=str, default="./data/trans_chinese_alpaca_data.json",help='the data used for instructing tuning')
-    parser.add_argument('--per_gpu_train_batch_size', default=1, type=int, help='Batch size per GPU/CPU for training.')
-    parser.add_argument('--gradient_accumulation_steps', default=4, type=int)
+    parser.add_argument('--per_gpu_train_batch_size', default=8, type=int, help='Batch size per GPU/CPU for training.')
+    parser.add_argument('--gradient_accumulation_steps', default=1, type=int)
     parser.add_argument('--epochs', default=1, type=int)
-    parser.add_argument('--learning_rate', default=3e-4, type=float)
+    parser.add_argument('--learning_rate', default=1e-4, type=float)
     parser.add_argument('--cutoff_len', default=256, type=int)
+    
     #PEFT arguments
     parser.add_argument('--lora_r', default=8, type=int)
     parser.add_argument('--lora_alpha', default=16, type=int)
@@ -135,6 +221,16 @@ if __name__ == "__main__":
     
     parser.add_argument('--output_dir', default='./saved_model', type=str)
     parser.add_argument('--instruction', default='', type=str)
+    
+    #Quantization arguments
+    parser.add_argument('--bits', default=4, type=int)
+    parser.add_argument('--full_finetune', default=False, type=bool)
+    parser.add_argument('--fp16', default=False, type=bool,help="Whether to use fp16 for training, fp16 is priority")
+    parser.add_argument('--bf16', default=True, type=bool,help="Whether to use bfloat16 for training")
+    parser.add_argument('--double_quant', default=True, type=bool,help="Whether to use double quantization for training")
+    parser.add_argument('--quant_type', default="nf4", type=str,choices=['fp4', 'nf4'],help="Whether to use double quantization for training")
+    parser.add_argument('--trust_remote_code', default=True, type=bool)
+    parser.add_argument('--gradient_checkpointing', default=True, type=bool)
 
     args, _ = parser.parse_known_args()
     print(args)
--- scripts/qlora_finetune_task1.sh
+++ scripts/qlora_finetune_task1.sh
@@ -5,24 +5,28 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && cd .. && pwd )"
 echo "working directory: ${DIR}"
 
-if [ -z "$OUTPUT_DIR" ]; then
-  OUTPUT_DIR="${DIR}/saved_model/task1_$(date +%F-%H%M.%S)"
-fi
 if [ -z "$DATA_DIR" ]; then
   DATA_DIR="${DIR}/data/task1_finetune.json"
 fi
-INSTRUCTION="给定一段描述空间的文本，其中包含空间语义异常的文本片段。请分析文本并找出其中的空间语义异常片段。空间语义异常是指描述物体、场景或事件在空间方面出现与常识或语义规则不符的表述。在给定的文本中，定位并标识出所有的空间语义异常片段。 \
-提示：异常文本片段的选取有以下六种情况：\n \
-6个文本片段形成两个完整的“空间实体(S)-空间方位(P)-事件(E)”三元组，对应的role为S1 P1 E1 S2 P2 E2。\n \
-5个文本片段形成一个完整的S-P-E三元组和一个不完整的三元组，不完整的三元组缺省了某个role。\n \
-4个文本片段形成两个不完整的S-P-E三元组，各缺省一个role。\n \
-3个文本片段形成一个完整的S-P-E三元组，对应的role为S1 P1 E1。\n \
-2个文本片段形成一个不完整的S-P-E三元组，缺省了某个role。\n \
-1个文本片段，role的取值是S1、P1或E1。\n \
-模型提供的判断结果应包含以下信息：role、text和idxes。role的取值包括S1、P1、E1、S2、P2和E2，其中S表示空间实体，P表示空间方位，E表示空间事件。当文本片段个数不大于3时，role的取值包括S1、P1和E1。text中包含了与role相对应的文本，而idxes则是文本中每个字符所对应的索引。输出的格式应为一个包含多个字典的列表，每个字典代表一个空间语义异常片段，包含role、text和idxes字段。请根据文本分析出的空间语义异常片段，按照给定的格式提供输出。" 
+# INSTRUCTION="给定一段描述空间的文本，其中包含空间语义异常的文本片段。请分析文本并找出其中的空间语义异常片段。空间语义异常是指描述物体、场景或事件在空间方面出现与常识或语义规则不符的表述。在给定的文本中，定位并标识出所有的空间语义异常片段。 \
+# 提示：异常文本片段的选取有以下六种情况：\n \
+# 6个文本片段形成两个完整的“空间实体(S)-空间方位(P)-事件(E)”三元组，对应的role为S1 P1 E1 S2 P2 E2。\n \
+# 5个文本片段形成一个完整的S-P-E三元组和一个不完整的三元组，不完整的三元组缺省了某个role。\n \
+# 4个文本片段形成两个不完整的S-P-E三元组，各缺省一个role。\n \
+# 3个文本片段形成一个完整的S-P-E三元组，对应的role为S1 P1 E1。\n \
+# 2个文本片段形成一个不完整的S-P-E三元组，缺省了某个role。\n \
+# 1个文本片段，role的取值是S1、P1或E1。\n \
+# 模型提供的判断结果应包含以下信息：role、text和idxes。role的取值包括S1、P1、E1、S2、P2和E2，其中S表示空间实体，P表示空间方位，E表示空间事件。当文本片段个数不大于3时，role的取值包括S1、P1和E1。text中包含了与role相对应的文本，而idxes则是文本中每个字符所对应的索引。输出的格式应为一个包含多个字典的列表，每个字典代表一个空间语义异常片段，包含role、text和idxes字段。请根据文本分析出的空间语义异常片段，按照给定的格式提供输出。" 
+INSTRUCTION="请分析文本并找出其中的空间语义异常片段:"
+
+lr=1e-6
+OUTPUT_DIR="${DIR}/saved_model/qlora/task1_$(date +%F-%H%M.%S)_${lr}"
 
 python3 -u finetune_qlora.py \
+--learning_rate ${lr} \
 --data "${DATA_DIR}" \
 --output_dir "${OUTPUT_DIR}" \
 --instruction "${INSTRUCTION}" \
 --epochs 1 \
+--lora_r 2 \
+--lora_alpha 4
